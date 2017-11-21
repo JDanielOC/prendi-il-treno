12565279455c4942fc0a586cd74953804517e1f5
Jason <vivanaranja@gmail.com>
2017-11-20 23:09:20 -0800

    basic styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,116 +1,115 @@
+// wait for the DOM
 $(document).ready(function () {
 
-
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyC1l8ng_f-j8pThYt6rjFgpeX8X3dVRo4s",
-    authDomain: "prendiiltreno.firebaseapp.com",
-    databaseURL: "https://prendiiltreno.firebaseio.com",
-    projectId: "prendiiltreno",
-    storageBucket: "prendiiltreno.appspot.com",
-    messagingSenderId: "106708772058"
-};
-firebase.initializeApp(config);
-
-// Create a variable to reference the database
-
-var database = firebase.database();
-
-// Capture Button Click
-$("#add-train").on("click", function () {
-    event.preventDefault();
-
-    // User Input
-    var trainName = $("#trainName-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var firstTrain = $("#firstTrain-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
-
-    //Create newTrain variable with train parameters
-    var newTrain = {
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC1l8ng_f-j8pThYt6rjFgpeX8X3dVRo4s",
+        authDomain: "prendiiltreno.firebaseapp.com",
+        databaseURL: "https://prendiiltreno.firebaseio.com",
+        projectId: "prendiiltreno",
+        storageBucket: "prendiiltreno.appspot.com",
+        messagingSenderId: "106708772058"
     };
+    firebase.initializeApp(config);
 
-    //push data to database object
-    database.ref().push(newTrain);
+    // Create a variable to reference the database
 
-    console.log(newTrain.trainName);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrain);
-    console.log(newTrain.frequency);
+    var database = firebase.database();
 
-    alert("Thank you for adding a new train to the schedule!");
+    // Capture Button Click
+    $("#add-train").on("click", function () {
+        event.preventDefault();
 
-    // clear form on submission
+        // User Input
+        var trainName = $("#trainName-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var firstTrain = $("#firstTrain-input").val().trim();
+        var frequency = $("#frequency-input").val().trim();
 
-    $("#trainName-input").val("");
-    $("#destination-input").val("");
-    $("#firstTrain-input").val("");
-    $("#frequency-input").val("");
+        //Create newTrain variable with train parameters
+        var newTrain = {
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency
+        };
 
-});
+        //push data to database object
+        database.ref().push(newTrain);
 
-// database listener - .on("value")
+        console.log(newTrain.trainName);
+        console.log(newTrain.destination);
+        console.log(newTrain.firstTrain);
+        console.log(newTrain.frequency);
 
-database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-    console.log(childSnapshot.val());
+        alert("Thank you for adding a new train to the schedule!");
 
-    var trainName = childSnapshot.val().trainName;
-    var destination = childSnapshot.val().destination;
-    var firstTrain = childSnapshot.val().firstTrain;
-    var frequency = childSnapshot.val().frequency;
+        // clear form on submission
 
-    //calculations of time
-    var firstTimeConverted = moment(firstTrain, "hh:mm:ss a").subtract(1, "years");
-    console.log(firstTimeConverted);
+        $("#trainName-input").val("");
+        $("#destination-input").val("");
+        $("#firstTrain-input").val("");
+        $("#frequency-input").val("");
 
-    var currentTime = moment();
-    console.log("Current Time: " + moment(currentTime).format("hh:mm:ss a"));
+    });
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // database listener - .on("value")
 
-    // Time apart (remainder)
-    var minLeft = diffTime % frequency;
-    console.log(minLeft);
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+            console.log(childSnapshot.val());
 
-    // Minute Until Train
-    var trainRemainder = frequency - minLeft;
-    console.log("MINUTES TILL TRAIN: " + trainRemainder);
+            var trainName = childSnapshot.val().trainName;
+            var destination = childSnapshot.val().destination;
+            var firstTrain = childSnapshot.val().firstTrain;
+            var frequency = childSnapshot.val().frequency;
 
-    // Next Train
-    var nextArrival = moment().add(trainRemainder, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+            //calculations of time
+            var firstTimeConverted = moment(firstTrain, "hh:mm:ss a").subtract(1, "years");
+            console.log(firstTimeConverted);
 
+            var currentTime = moment();
+            console.log("Current Time: " + moment(currentTime).format("hh:mm:ss a"));
 
-    //append data (and format time) to table on html page
-    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + moment(firstTrain, "HH:mm").format("hh:mm A") + "</td><td>" + frequency + "</td><td>" + moment(nextArrival).format("hh:mm A") + "</td><td>" + trainRemainder + "</td></tr>");
+            // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log("DIFFERENCE IN TIME: " + diffTime);
 
+            // Time apart (remainder)
+            var minLeft = diffTime % frequency;
+            console.log(minLeft);
 
-    //add setInterval to refresh trainTable every 10 seconds -- ISSUE: not sure what to load... the html page or the database?
-    // var auto_refresh;
+            // Minute Until Train
+            var trainRemainder = frequency - minLeft;
+            console.log("MINUTES TILL TRAIN: " + trainRemainder);
 
-    // function resetFunction() {
-    //     auto_refresh = setInterval(function () {
-    //          $('#trainTable').load('#trainTable');
-    //          }, 10000);
-    // };
-
-},
-
-
-
-// Create Error Handling
-
-function (errorObject) {
-    console.log(err);
+            // Next Train
+            var nextArrival = moment().add(trainRemainder, "minutes");
+            console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
 
-});
+            //append data (and format time) to table on html page
+            $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + moment(firstTrain, "HH:mm").format("hh:mm A") + "</td><td>" + frequency + "</td><td>" + moment(nextArrival).format("hh:mm A") + "</td><td>" + trainRemainder + "</td></tr>");
+
+
+            //add setInterval to refresh trainTable every 10 seconds -- ISSUE: not sure what to load... the html page or the database?
+            // var auto_refresh;
+
+            // function resetFunction() {
+            //     auto_refresh = setInterval(function () {
+            //          $('#trainTable').load('#trainTable');
+            //          }, 10000);
+            // };
+
+        },
+
+
+
+        // Create Error Handling
+
+        function (errorObject) {
+            console.log(err);
+
+
+        });
 
 });
